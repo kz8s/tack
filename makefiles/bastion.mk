3cf62d247a85eb6eff933c33a402cf180b31f164
jono <_@oj.io>
2016-02-10 15:00:58 -0800

    added: PHONY to bastion.mk

--- makefiles/bastion.mk
+++ makefiles/bastion.mk
@@ -6,3 +6,5 @@ plan-bastion: ; terraform plan -target module.bastion
 
 ssh: ## ssh into bastion host
 	@ssh -A core@`terraform output bastion-ip`
+
+.PHONY: apply-bastion destroy-bastion plan-bastion ssh
