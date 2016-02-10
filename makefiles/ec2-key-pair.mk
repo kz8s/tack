f2ae113ce46f77040b69558be2973b41ee98bf28
jono <_@oj.io>
2016-02-10 10:39:52 -0800

    added: key-pair adds to and deletes from auth agent

--- makefiles/ec2-key-pair.mk
+++ makefiles/ec2-key-pair.mk
@@ -7,9 +7,19 @@ $(DIR_KEY_PAIR)/$(AWS_EC2_KEY_NAME).pem: | $(DIR_KEY_PAIR)/
 		--output text \
 	> $@
 	chmod 400 $@
+	ssh-add $@
+
+.PHONY: create-key-pair
+create-key-pair: ## create ec2 key-pair and add to authentication agent
+create-key-pair: $(DIR_KEY_PAIR)/$(AWS_EC2_KEY_NAME).pem
 
 .PHONY: delete-key-pair
+delete-key-pair: ## delete ec2 key-pair and remove from authentication agent
 delete-key-pair:
 	aws --region ${AWS_REGION} ec2 delete-key-pair \
 		--key-name ${AWS_EC2_KEY_NAME}
+	ssh-add -L |grep "${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pem" \
+		> ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub
+	[ -s ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub ] &&\
+	 	ssh-add -d ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub
 	rm -rf $(DIR_KEY_PAIR)/
