7139a13c6a5c69f9b085db929a69388b250ca633
jono <_@oj.io>
2016-02-11 10:43:47 -0800

    added: more etcd smoke tests

--- makefiles/etcd.mk
+++ makefiles/etcd.mk
@@ -5,6 +5,8 @@ destroy-etcd: ; terraform destroy -target module.etcd
 plan-etcd: get ; terraform plan -target module.etcd
 
 test-etcd:
-	@ssh -A core@`terraform output bastion-ip` -- curl etcd1.k8s:2379/version
+	@ssh -A core@`terraform output bastion-ip` -- curl -sv etcd.k8s:2379/version
+	@ssh -A core@`terraform output bastion-ip` -- curl -sv etcd.k8s:2379/health
+	@ssh -A core@`terraform output bastion-ip` -- curl -sv etcd.k8s:2379/v2/members
 
 .PHONY: apply-etcd destroy-etcd plan-etcd test-etcd
