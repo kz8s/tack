cf4682de9a4373f62996e3dcd63d2ab7e8856d9e
jono <_@oj.io>
2016-02-11 06:20:26 -0800

    added: test-route53 target

--- makefiles/route53.mk
+++ makefiles/route53.mk
@@ -5,6 +5,10 @@ destroy-route53: ; terraform destroy -target module.route53
 plan-route53: get ; terraform plan -target module.route53
 
 test-route53:
-	@ssh -A core@`terraform output bastion-ip` -- nslookup etcd.k8s && dig k8s ANY
+	@ssh -A core@`terraform output bastion-ip` -- nslookup etcd.k8s
+	@ssh -A core@`terraform output bastion-ip` -- dig k8s ANY
+	@ssh -A core@`terraform output bastion-ip` -- dig +noall +answer SRV _etcd-server._tcp.k8s
+	@ssh -A core@`terraform output bastion-ip` -- dig +noall +answer SRV _etcd-client._tcp.k8s
+	@ssh -A core@`terraform output bastion-ip` -- dig +noall +answer etcd.k8s
 
-.PHONY: apply-route53 destroy-route53 plan-route53
+.PHONY: apply-route53 destroy-route53 plan-route53 test-route53
