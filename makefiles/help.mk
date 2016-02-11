61f867110b51258a1762f14d5a968575c157192a
jono <_@oj.io>
2016-02-11 07:08:56 -0800

    added: help.mk

---
+++ makefiles/help.mk
@@ -0,0 +1,15 @@
+## Display this help text
+help:
+	$(info Available targets)
+	@awk '/^[a-zA-Z\-\_0-9]+:/ {                    \
+		nb = sub( /^## /, "", helpMsg );              \
+		if(nb == 0) {                                 \
+			helpMsg = $$0;                              \
+			nb = sub( /^[^:]*:.* ## /, "", helpMsg );   \
+		}                                             \
+		if (nb)                                       \
+			print  $$1 "\t" helpMsg;                    \
+	}                                               \
+	{ helpMsg = $$0 }'                              \
+	$(MAKEFILE_LIST) | column -ts $$'\t' |          \
+	grep --color '^[^ ]*'
