f6e9afe94b4893a5a08fd8c5ba297cfd3d81ae35
jono <_@oj.io>
2016-02-13 06:07:19 -0800

    format: key-pair.mk

--- makefiles/key-pair.mk
+++ makefiles/key-pair.mk
@@ -16,13 +16,10 @@ create-key-pair: $(DIR_KEY_PAIR)/$(AWS_EC2_KEY_NAME).pem
 delete-key-pair:
 	# todo: rework this delete logic
 	aws --region ${AWS_REGION} ec2 delete-key-pair --key-name ${AWS_EC2_KEY_NAME}
-	ssh-add -L | grep "${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pem" \
-		> ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub
+	ssh-add -L | grep "${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pem" > ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub
 	if [ -a ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub ]; then \
 		ssh-add -d ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub; \
 		rm -rf $(DIR_KEY_PAIR)/; \
 	fi;
-	# @-ssh-add -d ${DIR_KEY_PAIR}/${AWS_EC2_KEY_NAME}.pub
-	# @-rm -rf $(DIR_KEY_PAIR)/
 
 .PHONY: create-key-pair delete-key-pair
