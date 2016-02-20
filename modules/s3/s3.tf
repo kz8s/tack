5a138143d71273f3be38d649ffd74c2138844adc
jono <_@oj.io>
2016-02-20 15:17:54 -0800

    refactor: moved kibe-proxy to manifests/common

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -10,12 +10,17 @@ resource "aws_s3_bucket" "ssl" {
   provisioner "local-exec" {
     command = <<LOCAL_EXEC
 aws s3 cp .ssl s3://${ var.bucket-prefix }/ssl --recursive --exclude "*" --include "*.tar" &&\
-# tar -C systemd -cf - . \
-#  | aws s3 cp - s3://${ var.bucket-prefix }/systemd/master.tar &&\
-tar -C manifests/system -cf - kube-apiserver.yml kube-podmaster.yml kube-proxy.yml \
+tar -cf - \
+  -C manifests/system kube-apiserver.yml kube-podmaster.yml \
+  -C ../common kube-proxy.yml \
   | aws s3 cp - s3://${ var.bucket-prefix }/manifests/etc.tar &&\
-tar -C manifests/system -cf - kube-controller-manager.yml kube-scheduler.yml \
-  | aws s3 cp - s3://${ var.bucket-prefix }/manifests/srv.tar
+tar -cf - \
+  -C manifests/system kube-controller-manager.yml kube-scheduler.yml \
+  | aws s3 cp - s3://${ var.bucket-prefix }/manifests/srv.tar &&\
+tar -cf -  \
+  -C manifests/common kube-proxy.yml \
+  -C ../worker kube-config.yml \
+  | aws s3 cp - s3://${ var.bucket-prefix }/manifests/worker.tar
 LOCAL_EXEC
   }
 }
