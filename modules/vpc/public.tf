c6bdfb281dd3dc776125894199a39eac1c9f9f3f
jono <_@oj.io>
2016-02-20 20:40:10 -0800

    decruft

--- modules/vpc/public.tf
+++ modules/vpc/public.tf
@@ -22,20 +22,6 @@ resource "aws_subnet" "public" {
   }
 }
 
-/*resource "aws_route_table" "public" {
-  vpc_id = "${ aws_vpc.main.id }"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${ aws_internet_gateway.main.id }"
-  }
-
-  tags {
-    Name = "k8s"
-    Cluster = "${ var.name }"
-  }
-}*/
-
 resource "aws_route" "public" {
   route_table_id = "${ aws_vpc.main.main_route_table_id }"
   destination_cidr_block = "0.0.0.0/0"
