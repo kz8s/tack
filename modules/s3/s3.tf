77345fe6bef1aefe19250bb1e0a1601a2f51c9c6
jono <_@oj.io>
2016-02-23 10:27:28 -0800

    formatting only: s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -11,9 +11,9 @@ resource "aws_s3_bucket" "ssl" {
   provisioner "local-exec" {
     command = <<LOCAL_EXEC
 aws s3 cp .ssl s3://${ var.bucket-prefix }/ssl --recursive --exclude "*" --include "*.tar" &&\
-tar -cf - \
-  -C manifests/system kube-apiserver.yml kube-podmaster.yml kube-proxy.yml \
-  | aws s3 cp - s3://${ var.bucket-prefix }/manifests/etc.tar &&\
+  tar -cf - \
+    -C manifests/system kube-apiserver.yml kube-podmaster.yml kube-proxy.yml \
+    | aws s3 cp - s3://${ var.bucket-prefix }/manifests/etc.tar &&\
 tar -cf - \
   -C manifests/system kube-controller-manager.yml kube-scheduler.yml \
   | aws s3 cp - s3://${ var.bucket-prefix }/manifests/srv.tar
