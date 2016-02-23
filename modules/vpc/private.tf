b16ed209b2b7558f6636fa3f6d3442d194be5e8a
jono <_@oj.io>
2016-02-23 12:22:43 -0800

    fixed: moved nat to public subnet

--- modules/vpc/private.tf
+++ modules/vpc/private.tf
@@ -4,7 +4,7 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [ "aws_internet_gateway.main" ]
 
   allocation_id = "${ aws_eip.nat.id }"
-  subnet_id = "${ aws_subnet.private.0.id }"
+  subnet_id = "${ aws_subnet.public.0.id }"
 }
 
 resource "aws_subnet" "private" {
