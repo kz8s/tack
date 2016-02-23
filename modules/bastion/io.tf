0fcb72b9607c4a8cb4daf8cdb18018133091c826
jono <_@oj.io>
2016-02-23 10:27:43 -0800

    decruft: bastion io.tf

--- modules/bastion/io.tf
+++ modules/bastion/io.tf
@@ -6,8 +6,6 @@ variable "key-name" {}
 variable "name" {}
 variable "security-group-id" {}
 variable "subnet-ids" {}
-/*variable "user-data" {}*/
 variable "vpc-id" {}
 
 output "ip" { value = "${ aws_instance.bastion.public_ip }" }
-/*output "security-group-id" { value = "${ aws_security_group.bastion.id }" }*/
