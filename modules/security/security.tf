1b08c4f39fb953c30f1c32d1652263f7b150aaa0
jono <_@oj.io>
2016-02-23 10:28:00 -0800

    formatting: security.tf

--- modules/security/security.tf
+++ modules/security/security.tf
@@ -1,7 +1,12 @@
 resource "aws_security_group" "bastion" {
-  name = "bastion"
-  description = "bastion security group"
-  vpc_id = "${ var.vpc-id }"
+  description = "k8s bastion security group"
+
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 
   ingress = {
     from_port = 22
@@ -10,85 +15,101 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = [ "${ var.cidr-allow-ssh }" ]
   }
 
+  name = "bastion"
+
+  tags {
+    Cluster = "${ var.name }"
+    Name = "bastion"
+    builtWith = "terraform"
+  }
+
+  vpc_id = "${ var.vpc-id }"
+}
+
+resource "aws_security_group" "etcd" {
+  description = "k8s etcd security group"
+
   egress = {
     from_port = 0
     to_port = 0
     protocol = "-1"
+    /*self = true*/
     cidr_blocks = [ "0.0.0.0/0" ]
   }
-}
-
-resource "aws_security_group" "etcd" {
-  name = "etcd"
-  description = "etcd security group"
-  vpc_id = "${ var.vpc-id }"
 
   ingress = {
     from_port = 0
     to_port = 0
     protocol = "-1"
     self = true
-    cidr_blocks = [ "10.0.0.0/16" ]
+    cidr_blocks = [ "${ var.cidr-vpc }" ]
   }
 
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    self = true
-    cidr_blocks = [ "0.0.0.0/0" ]
+  name = "etcd"
+
+  tags {
+    Cluster = "${ var.name }"
+    Name = "etcd"
+    builtWith = "terraform"
   }
+
+  vpc_id = "${ var.vpc-id }"
 }
 
 resource "aws_security_group" "external-elb" {
-  description = "External Master Load balancer"
-  name = "master-external-elb"
-  vpc_id = "${ var.vpc-id }"
-
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    cidr_blocks = [ "0.0.0.0/0", ]
-  }
+  description = "k8s master (apiserver) external elb"
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [ "${ var.cidr-vpc }", ]
+    cidr_blocks = [ "${ var.cidr-vpc }" ]
   }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  name = "master-external-elb"
 
   tags {
     Cluster = "${ var.name }"
     Name = "master-external-elb"
     builtWith = "terraform"
   }
+
+  vpc_id = "${ var.vpc-id }"
 }
 
 resource "aws_security_group" "worker" {
-  name = "worker"
-  vpc_id = "${ var.vpc-id }"
+  description = "k8s worker security group"
+
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    /*self = true*/
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 
   ingress = {
     from_port = 0
     to_port = 0
     protocol = "-1"
     self = true
-    cidr_blocks = [ "${ var.cidr-vpc }", ]
+    cidr_blocks = [ "${ var.cidr-vpc }" ]
   }
 
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    self = true
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
+  name = "worker"
 
   tags {
     Cluster = "${ var.name }"
     Name = "worker"
     builtWith = "terraform"
   }
+
+  vpc_id = "${ var.vpc-id }"
 }
