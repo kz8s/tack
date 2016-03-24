94ec1554f482ac630d7afa02828c3e19e0b09e4b
jono wells <_@oj.io>
2016-03-23 17:30:22 -0700

    refactor - moved init add ons to script

--- modules.tf
+++ modules.tf
@@ -129,12 +129,7 @@ resource "null_resource" "initialize" {
   }
 
   provisioner "local-exec" {
-    command = <<LOCALEXEC
-echo "✓ Polling for cluster life - this could take a minute or more"
-until echo "❤ trying to connect to cluster..." && kubectl cluster-info &>/dev/null; do sleep 7; done
-kubectl create -f manifests/addons
-kubectl create -f test/pods/busybox.yml
-kubectl get no
-LOCALEXEC
+    command = "./scripts/init-addons"
   }
+
 }
