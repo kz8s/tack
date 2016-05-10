57b703c813f406fcacaa00c4d9df807e1f7fbe73
jono wells <_@oj.io>
2016-05-10 13:37:56 -0700

    updated - etcd version check in make goal `test-etcd`
    etcdserver 2.2.0 -> 2.2.3

--- makefiles/etcd.mk
+++ makefiles/etcd.mk
@@ -1,6 +1,6 @@
 test-etcd:
 	@ssh -A core@`terraform output bastion-ip` '( curl -s etcd.k8s:2379/version )' \
-		| grep '{"etcdserver":"2.2.0","etcdcluster":"2.2.0"}'
+		| grep '{"etcdserver":"2.2.3","etcdcluster":"2.2.0"}'
 	@ssh -A core@`terraform output bastion-ip` '( curl -s etcd.k8s:2379/health )' \
 		| grep '{"health": "true"}'
 	@ssh -A core@`terraform output bastion-ip` '( curl -s etcd.k8s:2379/v2/members )' \
