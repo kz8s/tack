ddb7c4ed562fc595985663387eb9e88b25e0eeba
jono wells <_@oj.io>
2016-05-23 17:17:12 -0700

    fixes #15 - `--region` not passed to `aws s3 cp`
    tested on:
    - us-west-1
    - us-east-1

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -2,14 +2,21 @@ resource "aws_s3_bucket" "ssl" {
   acl = "private"
   bucket = "${ var.bucket-prefix }"
   force_destroy = true
+
   tags {
+    builtWith = "terraform"
     Cluster = "${ var.name }"
     Name = "k8s"
-    builtWith = "terraform"
   }
 
   provisioner "local-exec" {
-    command = "HYPERKUBE=${ var.hyperkube-image } PODMASTER=${ var.podmaster-image } ${ path.module }/s3-cp ${ var.bucket-prefix }"
+    command = <<EOF
+      HYPERKUBE=${ var.hyperkube-image } \
+      PODMASTER=${ var.podmaster-image } \
+      REGION=${ var.region } \
+        ${ path.module }/s3-cp ${ var.bucket-prefix }
+EOF
   }
 
+  region = "${ var.region }"
 }
