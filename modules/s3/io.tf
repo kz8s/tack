89775093724bc9560fc504e4fc04e73746b28a42
Jono Wells <on@oj.io>
2016-05-25 08:44:57 -0700

    fixes #15 - `--region` not passed to `aws s3 cp` (#16)
    tested on:
    - us-west-1
    - us-east-1

--- modules/s3/io.tf
+++ modules/s3/io.tf
@@ -3,5 +3,6 @@ variable "hyperkube-image" {}
 variable "k8s-version" {}
 variable "name" {}
 variable "podmaster-image" {}
+variable "region" {}
 
 output "bucket-prefix" { value = "${ var.bucket-prefix }" }
