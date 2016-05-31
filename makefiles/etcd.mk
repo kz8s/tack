813262a4da08d65362a9409501024ffac3a97931
jono wells <_@oj.io>
2016-05-31 12:38:59 -0700

    updated - make goal `test-etcd`
    - to use versions: etcdserver=2.3.1 and etcdcluster=2.3.0
    - removed output suppression '@' from makefile

--- makefiles/etcd.mk
+++ makefiles/etcd.mk
@@ -1,9 +1,12 @@
 test-etcd:
-	@ssh -A core@`terraform output bastion-ip` '( curl -s etcd.k8s:2379/version )' \
-		| grep '{"etcdserver":"2.2.3","etcdcluster":"2.2.0"}'
-	@ssh -A core@`terraform output bastion-ip` '( curl -s etcd.k8s:2379/health )' \
+	ssh -A core@`terraform output bastion-ip` \
+		'( curl -s http://etcd.k8s:2379/version )' \
+		| grep '{"etcdserver":"2.3.1","etcdcluster":"2.3.0"}'
+	ssh -A core@`terraform output bastion-ip` \
+		'( curl -s http://etcd.k8s:2379/health )' \
 		| grep '{"health": "true"}'
-	@ssh -A core@`terraform output bastion-ip` '( curl -s etcd.k8s:2379/v2/members )' \
+	ssh -A core@`terraform output bastion-ip` \
+		'( curl -s http://etcd.k8s:2379/v2/members )' \
 		| grep -o '"name":' | wc -l | grep 3
 
 .PHONY: test-etcd
