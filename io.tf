f4c399a1a7854a886ffcf14ccbabf7c68ca979cf
jono wells <jono@kz8s.io>
2016-07-08 10:41:00 -0700

    bumped - k8s version to 1.3.0

--- io.tf
+++ io.tf
@@ -33,8 +33,8 @@ variable "instance-type" {
 variable "internal-tld" { default = "k8s" }
 variable "k8s" {
   default = {
-    hyperkube-image = "gcr.io/google_containers/hyperkube:v1.2.5"
-    version = "v1.2.5"
+    hyperkube-image = "gcr.io/google_containers/hyperkube:v1.3.0"
+    version = "v1.3.0"
   }
 }
 variable "name" {}
