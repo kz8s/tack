adaa14e752c795fcb3303d7f058ed0d014441074
Adam Savitzky <adam.savitzky@gmail.com>
2016-10-24 12:11:46 -0700

    Attach EBS volumes to etcd nodes for persistence
    If you ever lose your etcd cluster for whatever reason, or if you should ever need to restart it, you should be able to recover your state. Mentioned in this issue: https://github.com/kz8s/tack/issues/75

--- modules/etcd/ec2.tf
+++ modules/etcd/ec2.tf
@@ -1,3 +1,30 @@
+resource "aws_ebs_volume" "etcd" {
+  count = "${ length( split(",", var.azs) ) }"
+
+  availability_zone = "${ element( split(",", var.azs), 0 ) }"
+
+  type = "gp2"
+  size = 100
+
+  tags {
+    builtWith = "terraform"
+    Cluster = "${ var.name }"
+    depends-id = "${ var.depends-id }"
+    KubernetesCluster = "${ var.name }"
+    Name = "etcd${ count.index + 1 }-${ var.name }"
+    role = "etcd,apiserver"
+    version = "${ var.coreos-hyperkube-tag }"
+  }
+}
+
+resource "aws_volume_attachment" "etcd" {
+  count = "${ length( split(",", var.azs) ) }"
+
+  device_name = "/dev/xvdf"
+  volume_id = "${ element(aws_ebs_volume.etcd.*.id, count.index) }"
+  instance_id = "${ element(aws_instance.etcd.*.id, count.index) }"
+}
+
 resource "aws_instance" "etcd" {
   count = "${ length( split(",", var.etcd-ips) ) }"
 
@@ -32,5 +59,9 @@ resource "aws_instance" "etcd" {
 }
 
 resource "null_resource" "dummy_dependency" {
-  depends_on = [ "aws_instance.etcd" ]
+  depends_on = [
+    "aws_instance.etcd",
+    "aws_ebs_volume.etcd",
+    "aws_volume_attachment.etcd"
+  ]
 }
