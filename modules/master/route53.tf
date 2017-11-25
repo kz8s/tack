69503e506e01afb23201bef903e196938019a4dc
Victor Unegbu <victor@unegbu.com>
2017-11-25 14:15:13 -0600

    add apiserver A records in k8s master module

--- modules/master/route53.tf
+++ modules/master/route53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "CNAME-master" {
   type = "CNAME"
   zone_id = "${ var.internal-zone-id }"
 }
+
+resource "aws_route53_record" "A-apiserver" {
+  name = "apiserver"
+  records = [ "${ aws_instance.apiserver.*.private_ip }" ]
+  ttl = "60"
+  type = "A"
+  zone_id = "${ var.internal-zone-id }"
+}
